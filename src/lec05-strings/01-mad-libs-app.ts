95d2a62dbb23cdf03ff1637170fdc8ffc977ceb9
Jeffrey-Young <youngjt@live.unc.edu>
2018-01-30 21:14:38 -0500

    initial commit for lec5

---
+++ src/lec05-strings/01-mad-libs-app.ts
@@ -0,0 +1,41 @@
+import { print, promptString } from "introcs";
+
+export let main = async () => {
+    
+    // Part 1:
+
+    // TODO: Prompt the user for a positive and negative adjective
+
+    let positiveAdj: string = "";
+    let negativeAdj: string = "";
+
+    // TODO: Prompt the user for a verb
+
+    let verb: string = "";
+    
+    // TODO: Prompt the user for a large and small number
+
+    let uncScore: number = 0;
+    let dookScore: number = 0;
+    
+    // TODO: Compute the difference between the two scores in a number variable called winBy
+
+    let winBy: number = 0;
+
+    /* ***Check in on Poll Everywhere*** */
+
+    // Part 2:
+
+    let madLib1: string = "";
+    let madLib2: string = "";
+    let madLib3: string = "";
+
+    // Concatenate the individual parts into finalMadLib
+
+    let finalMadLib: string = "";
+
+    // Print the finalMadLib
+    
+    
+
+};
