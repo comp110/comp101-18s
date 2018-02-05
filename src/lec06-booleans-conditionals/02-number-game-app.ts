c0c7ea1835f8490b4ecea0d085a3f0aca4e48eff
Jeffrey T Young <youngjt@cs.unc.edu>
2018-02-05 10:17:14 -0500

    comment fix

--- src/lec06-booleans-conditionals/02-number-game-app.ts
+++ src/lec06-booleans-conditionals/02-number-game-app.ts
@@ -5,6 +5,8 @@ export let main = async () => {
     let guess: number = await promptNumber("Guess a number!");
     
     // TODO: Write an if-then-else statement
+    // if the number is equal to 42 print correct,
+    // else print wrong
 
 };
 
