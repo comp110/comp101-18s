d64abfdb64b8f01177d5d8c7ea36d66b8805752d
Kris Jordan <krisjordan@gmail.com>
2018-02-07 08:58:42 -0500

    Lec 07 examples added

---
+++ src/lec07-if-then-else/00-random-app.ts
@@ -0,0 +1,15 @@
+import { print, random } from "introcs";
+
+export let main = async () => {
+
+    // TODO 1: Declare a number variable named choice
+
+    // TODO 2: Initialize choice to be assigned random(0, 100)
+
+    // TODO 3: Print the value of the choice variable
+
+    // TODO 4: Try refreshing your browser a few times
+
+};
+
+main();
