89e431653e79e2eb196554aac8c4b600a4a7cfdc
Kris Jordan <krisjordan@gmail.com>
2018-04-02 10:12:02 -0400

    lecture 19

---
+++ src/lec19-classes-objects/00-data-objects-app.ts
@@ -0,0 +1,13 @@
+import { print, csvToArray } from "introcs";
+
+// TODO #1 Declare a Rectangle Class
+
+export let main = async () => {
+
+    // TODO #2 Declare a Rectangle Variable
+    
+};
+
+// TODO #3 Write an area function
+
+main();
