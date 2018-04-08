38dcceebf0e9afde068cb5ebd767a2c95ad187bd
Jeffrey-Young <youngjt@live.unc.edu>
2018-04-08 17:39:22 -0400

    bump

---
+++ src/lec21-shapes/00-point-app.ts
@@ -0,0 +1,14 @@
+import { print } from "introcs";
+
+class Point {
+    x: number = 0;
+    y: number = 0;
+}
+
+export let main = async () => {
+
+};
+
+// TODO: Implement a midpoint function
+
+main();
