8650dfe2073c42c589f3d955d2e52b9517f7d664
Kris Jordan <krisjordan@gmail.com>
2018-04-18 10:38:53 -0400

    Add lecture 23 file

---
+++ src/lec23-object-practice/01-waffle-iron-app.ts
@@ -0,0 +1,16 @@
+import { print, promptNumber } from "introcs";
+
+class Waffle {
+    mix: number = 0;
+    eggs: number = 0;
+    oil: number = 0;
+    cooked: boolean = false;
+}
+
+export let main = async () => {
+    // TODO: Use your functions here
+};
+
+// TODO: Define your functions here
+
+main();
